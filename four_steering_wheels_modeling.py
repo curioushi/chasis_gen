a8970e6a864144435befd4a6030c33aa9d4e6440
xyz <xyz@xyzrobotics.com>
2023-06-27 18:34:50 +0800

    add physics

--- four_steering_wheels_modeling.py
+++ four_steering_wheels_modeling.py
@@ -3,6 +3,8 @@ import sys
 import bpy
 import math
 import yaml
+import cv2
+import numpy as np
 import argparse
 
 def remove_all_cameras():
@@ -29,6 +31,15 @@ def triangulate(obj):
     bpy.ops.mesh.quads_convert_to_tris(quad_method='BEAUTY', ngon_method='BEAUTY')
     bpy.ops.object.editmode_toggle()
 
+def bevel(obj, offset):
+    """Bevel the edges of an object"""
+    bpy.ops.object.select_all(action='DESELECT')
+    obj.select_set(True)
+    bpy.ops.object.editmode_toggle()
+    bpy.ops.mesh.select_all(action='SELECT')
+    bpy.ops.mesh.bevel(offset=offset, offset_pct=0, affect='EDGES')
+    bpy.ops.object.editmode_toggle()
+
 
 def create_wheel(name, diameter, thickness, location, padding=0.01):
     """Create a wheel object"""
@@ -36,7 +47,7 @@ def create_wheel(name, diameter, thickness, location, padding=0.01):
     bpy.ops.transform.rotate(value=math.pi / 2, orient_axis='X')
     wheel.blender_obj.name = name
     wheel.blender_obj.data.name = name + '_mesh'
-    wheel_bound = bproc.object.create_primitive('CYLINDER', vertices=360, radius=padding+math.hypot(diameter/2, thickness/2), depth=diameter, location=location)
+    wheel_bound = bproc.object.create_primitive('CYLINDER', vertices=360, radius=padding+math.hypot(diameter/2, thickness/2), depth=padding+diameter, location=location)
     return wheel, wheel_bound
 
 def create_axis(name, location):
@@ -44,6 +55,14 @@ def create_axis(name, location):
     axis = bpy.context.selected_objects[0]
     axis.name = name
     return axis
+
+def create_checker_texture(filepath, color1=(0, 0, 0), color2=(1, 1, 1), size=5, grid_size=64):
+    tex = np.zeros((grid_size*size, grid_size*size, 3), dtype=float)
+    for i in range(size):
+        for j in range(size):
+            tex[i*grid_size:(i+1)*grid_size, j*grid_size:(j+1)*grid_size] = color1 if (i+j) % 2 == 0 else color2
+    tex = (tex * 255).astype(np.uint8)
+    cv2.imwrite(filepath, tex)
 
 
 def main():
@@ -63,10 +82,10 @@ def main():
         wheel_thickness = 0.060 # m
         wheel_base_x = 0.490 # m
         wheel_base_y = 0.370 # m
+        wheel_bevel = 0.01 # m
         chasis_width = 0.5 # m
         chasis_length = 0.738 # m
-        chasis_height = 0.233 # m
-        chasis_z_offset = 0.107 # m
+        chasis_height = 0.140 # m
     else:
         with open(args.config) as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
@@ -76,10 +95,10 @@ def main():
             wheel_thickness = config['wheel_thickness']
             wheel_base_x = config['wheel_base_x']
             wheel_base_y = config['wheel_base_y']
+            wheel_bevel = config['wheel_bevel']
             chasis_width = config['chasis_width']
             chasis_length = config['chasis_length']
             chasis_height = config['chasis_height']
-            chasis_z_offset = config['chasis_z_offset']
 
     print('-' * 80)
     print('chasis_name: ' + chasis_name)
@@ -87,10 +106,10 @@ def main():
     print('wheel_thickness: ' + str(wheel_thickness))
     print('wheel_base_x: ' + str(wheel_base_x))
     print('wheel_base_y: ' + str(wheel_base_y))
+    print('wheel_bevel: ' + str(wheel_bevel))
     print('chasis_width: ' + str(chasis_width))
     print('chasis_length: ' + str(chasis_length))
     print('chasis_height: ' + str(chasis_height))
-    print('chasis_z_offset: ' + str(chasis_z_offset))
     print('-' * 80)
 
     bproc.init()
@@ -98,23 +117,49 @@ def main():
     remove_all_cameras()
 
     chasis_axis = create_axis(chasis_name, (0, 0, 0))
-    chasis = bproc.object.create_primitive('CUBE', scale = (chasis_length / 2, chasis_width/2, chasis_height/2), location = (0, 0, chasis_z_offset + chasis_height / 2))
+    chasis = bproc.object.create_primitive('CUBE', scale = (chasis_length / 2, chasis_width/2, chasis_height/2), location = (0, 0, wheel_diameter + chasis_height / 2))
     chasis.blender_obj.name = 'chasis'
     chasis.blender_obj.data.name = 'chasis_mesh'
-    steer1 = create_axis('steer_left_front', (wheel_base_x / 2, wheel_base_y / 2, wheel_diameter / 2))
-    steer2 = create_axis('steer_left_back', (-wheel_base_x / 2, wheel_base_y / 2, wheel_diameter / 2))
-    steer3 = create_axis('steer_right_back', (-wheel_base_x / 2, -wheel_base_y / 2, wheel_diameter / 2))
-    steer4 = create_axis('steer_right_front', (wheel_base_x / 2, -wheel_base_y / 2, wheel_diameter / 2))
+    steer1, steer1_bound = create_wheel('steer_left_front', wheel_diameter / 2, wheel_thickness * 1.2, (wheel_base_x / 2, wheel_base_y / 2, wheel_diameter / 2))
+    steer2, steer2_bound = create_wheel('steer_left_back', wheel_diameter / 2, wheel_thickness * 1.2, (-wheel_base_x / 2, wheel_base_y / 2, wheel_diameter / 2))
+    steer3, steer3_bound = create_wheel('steer_right_back', wheel_diameter / 2, wheel_thickness * 1.2, (-wheel_base_x / 2, -wheel_base_y / 2, wheel_diameter / 2))
+    steer4, steer4_bound = create_wheel('steer_right_front', wheel_diameter / 2, wheel_thickness * 1.2, (wheel_base_x / 2, -wheel_base_y / 2, wheel_diameter / 2))
     wheel1, wheel1_bound = create_wheel('wheel_left_front', wheel_diameter, wheel_thickness, (wheel_base_x / 2, wheel_base_y / 2, wheel_diameter / 2))
     wheel2, wheel2_bound = create_wheel('wheel_left_back', wheel_diameter, wheel_thickness, (-wheel_base_x / 2, wheel_base_y / 2, wheel_diameter / 2))
     wheel3, wheel3_bound = create_wheel('wheel_right_back', wheel_diameter, wheel_thickness, (-wheel_base_x / 2, -wheel_base_y / 2, wheel_diameter / 2))
     wheel4, wheel4_bound = create_wheel('wheel_right_front', wheel_diameter, wheel_thickness, (wheel_base_x / 2, -wheel_base_y / 2, wheel_diameter / 2))
 
-    boolean_modify(chasis.blender_obj, wheel1_bound.blender_obj, 'DIFFERENCE')
-    boolean_modify(chasis.blender_obj, wheel2_bound.blender_obj, 'DIFFERENCE')
-    boolean_modify(chasis.blender_obj, wheel3_bound.blender_obj, 'DIFFERENCE')
-    boolean_modify(chasis.blender_obj, wheel4_bound.blender_obj, 'DIFFERENCE')
+    create_checker_texture('/tmp/chasis_texture.png', (205/255, 117/255, 149/255), (0, 0, 0), 20, 64)
+    chasis_mat = bproc.material.create_material_from_texture('/tmp/chasis_texture.png', 'chasis_mat')
+    create_checker_texture('/tmp/wheel_texture.png', (182/255, 172/255, 77/255), (0, 0, 0), 10, 64)
+    wheel_mat = bproc.material.create_material_from_texture('/tmp/wheel_texture.png', 'wheel_mat')
+    create_checker_texture('/tmp/steer_texture.png', (101/255, 138/255, 255/255), (0, 0, 0), 4, 64)
+    steer_mat = bproc.material.create_material_from_texture('/tmp/steer_texture.png', 'steer_mat')
+    chasis.add_material(chasis_mat)
+    wheel1.add_material(wheel_mat)
+    wheel2.add_material(wheel_mat)
+    wheel3.add_material(wheel_mat)
+    wheel4.add_material(wheel_mat)
+    steer1.add_material(steer_mat)
+    steer2.add_material(steer_mat)
+    steer3.add_material(steer_mat)
+    steer4.add_material(steer_mat)
 
+    bevel(wheel1.blender_obj, wheel_bevel)
+    bevel(wheel2.blender_obj, wheel_bevel)
+    bevel(wheel3.blender_obj, wheel_bevel)
+    bevel(wheel4.blender_obj, wheel_bevel)
+
+    # unknown bug: UNION <-> DIFFERENCE
+    boolean_modify(wheel1.blender_obj, steer1.blender_obj, 'UNION')
+    boolean_modify(wheel2.blender_obj, steer2.blender_obj, 'UNION')
+    boolean_modify(wheel3.blender_obj, steer3.blender_obj, 'UNION')
+    boolean_modify(wheel4.blender_obj, steer4.blender_obj, 'UNION')
+
+    steer1_bound.delete(True)
+    steer2_bound.delete(True)
+    steer3_bound.delete(True)
+    steer4_bound.delete(True)
     wheel1_bound.delete(True)
     wheel2_bound.delete(True)
     wheel3_bound.delete(True)
@@ -131,10 +176,10 @@ def main():
     wheel2.blender_obj.parent = chasis_axis
     wheel3.blender_obj.parent = chasis_axis
     wheel4.blender_obj.parent = chasis_axis
-    steer1.parent = chasis_axis
-    steer2.parent = chasis_axis
-    steer3.parent = chasis_axis
-    steer4.parent = chasis_axis
+    steer1.blender_obj.parent = chasis_axis
+    steer2.blender_obj.parent = chasis_axis
+    steer3.blender_obj.parent = chasis_axis
+    steer4.blender_obj.parent = chasis_axis
 
     bpy.ops.wm.usd_export(filepath=args.output)
 
--- four_steering_wheels_set_physics.py
+++ four_steering_wheels_set_physics.py
@@ -4,11 +4,12 @@ import numpy as np
 simulation_app = SimulationApp({"headless": False})
 
 import omni
-from pxr import Usd, UsdGeom, UsdPhysics
+from pxr import Usd, UsdGeom, UsdPhysics, PhysxSchema, Sdf
 from omni.isaac.core import World
 from omni.isaac.core.utils.stage import open_stage
 from omni.physxcommands import SetRigidBodyCommand, CreateJointCommand
 from omni.physxcommands import ApplyAPISchemaCommand
+from omni.usd.commands.usd_commands import MovePrimCommand
 
 success = open_stage(usd_path='RangerMini2.usda')
 world = World(stage_units_in_meters=1.0)
@@ -17,6 +18,7 @@ world.scene.add_default_ground_plane()
 
 stage = omni.usd.get_context().get_stage()
 
+joints = stage.DefinePrim('/RangerMini2/joints', 'Xform')
 robot = stage.GetPrimAtPath('/RangerMini2')
 chasis = stage.GetPrimAtPath('/RangerMini2/chasis')
 steer_left_front = stage.GetPrimAtPath('/RangerMini2/steer_left_front')
@@ -27,9 +29,10 @@ wheel_left_front = stage.GetPrimAtPath('/RangerMini2/wheel_left_front')
 wheel_left_back = stage.GetPrimAtPath('/RangerMini2/wheel_left_back')
 wheel_right_front = stage.GetPrimAtPath('/RangerMini2/wheel_right_front')
 wheel_right_back = stage.GetPrimAtPath('/RangerMini2/wheel_right_back')
+material = stage.GetPrimAtPath('/_materials')
 
 # setup rigid body and collision
-for obj in [chasis, wheel_left_front, wheel_left_back, wheel_right_front, wheel_right_back]:
+for obj in [chasis, steer_left_front, steer_left_back, steer_right_front, steer_right_back, wheel_left_front, wheel_left_back, wheel_right_front, wheel_right_back]:
     obj_path = obj.GetPath()
     obj_name = obj.GetName()
     SetRigidBodyCommand(obj_path).do()
@@ -37,23 +40,56 @@ for obj in [chasis, wheel_left_front, wheel_left_back, wheel_right_front, wheel_
     mesh = stage.GetPrimAtPath(obj_path.AppendPath(obj_name + '_mesh'))
     mesh.GetAttribute('physics:approximation').Set('none')
 
-joint = CreateJointCommand(stage, 'Revolute', chasis, steer_left_front).do()
-joint.GetProperty('physics:axis').Set('Z')
-joint = CreateJointCommand(stage, 'Revolute', chasis, steer_left_back).do()
-joint.GetProperty('physics:axis').Set('Z')
-joint = CreateJointCommand(stage, 'Revolute', chasis, steer_right_front).do()
-joint.GetProperty('physics:axis').Set('Z')
-joint = CreateJointCommand(stage, 'Revolute', chasis, steer_right_back).do()
-joint.GetProperty('physics:axis').Set('Z')
+chasis.GetProperty('physics:mass').Set(40)
+for steer in [steer_left_front, steer_left_back, steer_right_front, steer_right_back]:
+    steer.GetProperty('physics:mass').Set(1)
+for wheel in [wheel_left_front, wheel_left_back, wheel_right_front, wheel_right_back]:
+    wheel.GetProperty('physics:mass').Set(2)
 
-joint = CreateJointCommand(stage, 'Revolute', steer_left_front, wheel_left_front).do()
-joint.GetProperty('physics:axis').Set('Z')
-joint = CreateJointCommand(stage, 'Revolute', steer_left_back, wheel_left_back).do()
-joint.GetProperty('physics:axis').Set('Z')
-joint = CreateJointCommand(stage, 'Revolute', steer_right_front, wheel_right_front).do()
-joint.GetProperty('physics:axis').Set('Z')
-joint = CreateJointCommand(stage, 'Revolute', steer_right_back, wheel_right_back).do()
-joint.GetProperty('physics:axis').Set('Z')
+# steering joints
+def create_steer_joint(name, parent, child):
+    global stage, joints
+    joint = CreateJointCommand(stage, 'Revolute', parent, child).do()
+    joint.GetProperty('physics:axis').Set('Y')
+    MovePrimCommand(joint.GetPath(), joints.GetPath().AppendPath(name)).do()
+    joint = stage.GetPrimAtPath(joints.GetPath().AppendPath(name))
+    UsdPhysics.DriveAPI.Apply(joint, UsdPhysics.Tokens.angular)
+    joint.GetProperty('drive:angular:physics:type').Set('acceleration')
+    joint.GetProperty('drive:angular:physics:stiffness').Set(350)
+    joint.GetProperty('drive:angular:physics:damping').Set(100)
+    PhysxSchema.JointStateAPI.Apply(joint.GetPrim(), "angular")
+    return joint
+
+joint_steer_left_front = create_steer_joint('joint_steer_left_front', chasis, steer_left_front)
+joint_steer_left_back = create_steer_joint('joint_steer_left_back', chasis, steer_left_back)
+joint_steer_right_front = create_steer_joint('joint_steer_right_front', chasis, steer_right_front)
+joint_steer_right_back = create_steer_joint('joint_steer_right_back', chasis, steer_right_back)
+
+# wheel joints
+def create_wheel_joint(name, parent, child):
+    global stage, joints
+    joint = CreateJointCommand(stage, 'Revolute', parent, child).do()
+    joint.GetProperty('physics:axis').Set('Z')
+    MovePrimCommand(joint.GetPath(), joints.GetPath().AppendPath(name)).do()
+    joint = stage.GetPrimAtPath(joints.GetPath().AppendPath(name))
+    UsdPhysics.DriveAPI.Apply(joint, UsdPhysics.Tokens.angular)
+    joint.GetProperty('drive:angular:physics:type').Set('acceleration')
+    joint.GetProperty('drive:angular:physics:damping').Set(1)
+    joint.GetProperty('drive:angular:physics:stiffness').Set(20)
+    joint.CreateAttribute('physxJoint:maxJointVelocity', Sdf.ValueTypeNames.Float).Set(10)
+    PhysxSchema.JointStateAPI.Apply(joint.GetPrim(), "angular")
+    return joint
+
+joint_wheel_left_front = create_wheel_joint('joint_wheel_left_front', steer_left_front, wheel_left_front)
+joint_wheel_left_back = create_wheel_joint('joint_wheel_left_back', steer_left_back, wheel_left_back)
+joint_wheel_right_front = create_wheel_joint('joint_wheel_right_front', steer_right_front, wheel_right_front)
+joint_wheel_right_back = create_wheel_joint('joint_wheel_right_back', steer_right_back, wheel_right_back)
+
+ApplyAPISchemaCommand(UsdPhysics.ArticulationRootAPI, robot).do()
+ApplyAPISchemaCommand(PhysxSchema.PhysxArticulationAPI, robot).do()
+robot.GetAttribute('physxArticulation:enabledSelfCollisions').Set(False)
+
+MovePrimCommand(material.GetPath(), robot.GetPath().AppendPath('_materials')).do()
 
 stage.Export('RangerMini2_physics.usda')
 
